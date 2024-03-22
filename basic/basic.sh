afb59dd73e2d89b7d0d35c96359cb049437a0cb5
MD Abu Saleh <100086418+mdabusaleh1@users.noreply.github.com>
2024-03-22 19:49:58 +0600

    basic termux Command

---
+++ basic/basic.sh
@@ -0,0 +1,64 @@
+apt update
+apt upgrade
+pkg install python -y
+pkg install python2 -y
+pkg install git -y
+pkg install figlet -y
+pkg install toilet -y
+pkg install nano -y
+pkg install php 
+pip2 install requests
+pip install future
+apt install ruby
+apt install openssh
+apt install wget
+apt install curl
+pkg install python
+pkg install python2
+pkg install bash
+pkg install php
+pkg install perl
+pkg inatall curl
+pkg install ruby
+pkg install nano
+pkg install ngrok
+pkg install nodejs
+pkg install nmap
+pkg install wget
+pkg install grep
+pkg install golang
+pkg install openssh
+pkg install vim
+pkg install w3m
+pkg install toilet
+pkg install w3m
+pkg install git
+pkg Install bs4
+pkg Install npm
+pkg Install emacs
+pkg Install micro
+pkg Install figlet
+pkg Install clang
+pkg Install coreutils
+pkg Install tqdm
+pkg Install gnupg
+pkg Install swi-prolog
+pkg Install clib
+pkg Install rdf
+pkg Install chr
+pkg Install http
+pkg Install pcre
+pkg Install pengines
+pkg Install protobufs
+pkg Install sgml
+pkg Install yaml
+pkg Install zlib
+pkg Install ssl
+pkg Install semweb
+pkg Install bdb
+pkg Install odbc
+pkg Install space
+pkg Install sl
+pkg Install top
+pkg install sudo
+pkg install tsu
